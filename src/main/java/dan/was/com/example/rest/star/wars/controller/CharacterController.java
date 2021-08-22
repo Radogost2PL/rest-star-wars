32f40e9278e5de2522ca6e36981bada1653464da
Radogost2PL <d.wasil@protonmail.com>
2021-08-22 21:07:18 +0200

    repair ExceptionResolver rest response

--- src/main/java/dan/was/com/example/rest/star/wars/controller/CharacterController.java
+++ src/main/java/dan/was/com/example/rest/star/wars/controller/CharacterController.java
@@ -16,7 +16,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EnableWebMvc
 @RequiredArgsConstructor
 @RestController
 public class CharacterController {
